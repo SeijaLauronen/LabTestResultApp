14d196dc3ad17a720fcb003c0977815e0c679b22
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2025-11-12 20:36:12 +0200

    Added frontend to the main repo.

---
+++ frontend/src/components/ImportResults.tsx
@@ -0,0 +1,8 @@
+// SL 202511: Import results as text (copy - paste) component
+import React from "react";
+
+const ImportResults = () => {
+  return <div>Tuontikomponentti tulossa tänne</div>;
+};
+
+export default ImportResults;
